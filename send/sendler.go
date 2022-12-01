d27addcade187ea563e296e1a5caefef990f0565
Tkachenkoegor <Egorh16@mail.ru>
2022-12-01 10:44:21 +0300

    added sendler to rabbit

--- send/sendler.go
+++ send/sendler.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/joho/godotenv"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 	"os"
+	"time"
 )
 
 func failOnError(err error, msg string) {
@@ -31,4 +34,33 @@ func main() {
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	msgBody := `{"collectionName":"testCollection#2",
+			"message":"test message2",
+			"date":"66329999999999999"}`
+	fmt.Println(msgBody)
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(msgBody),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", msgBody)
+
 }
